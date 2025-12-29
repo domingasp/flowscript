e5b71e9e7567a9007d9d2f027c26fc2dfd4b9149
Domingas Pleckauskas <d.pleck@outlook.com>
2025-12-29 16:18:49 +0000

    Add perfectionist

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,18 +1,19 @@
 import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import pluginVue from "eslint-plugin-vue";
 import boundaries from "eslint-plugin-boundaries";
 import { strict as boundariesStrict } from "eslint-plugin-boundaries/config";
+import perfectionist from "eslint-plugin-perfectionist";
+import pluginVue from "eslint-plugin-vue";
 import { defineConfig, globalIgnores } from "eslint/config";
+import globals from "globals";
+import tseslint from "typescript-eslint";
 
 export default defineConfig([
-  globalIgnores(["src-tauri/**", "**/*.d.ts"]),
+  globalIgnores(["src-tauri/**", "**/*.d.ts", "*.config.ts"]),
   {
-    files: ["**/*.{js,mjs,cjs,ts,mts,cts,vue}"],
-    plugins: { js },
     extends: ["js/recommended"],
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,vue}"],
     languageOptions: { globals: globals.browser },
+    plugins: { js },
   },
   tseslint.configs.recommended,
   pluginVue.configs["flat/essential"],
@@ -39,26 +40,6 @@ export default defineConfig([
   {
     files: ["**/*.{ts,vue}"],
     plugins: { boundaries },
-    settings: {
-      ...boundariesStrict.settings,
-      "boundaries/elements": [
-        { type: "app", pattern: "src/**" },
-        { type: "components", pattern: "components/**" },
-        {
-          capture: ["featureName"],
-          pattern: "features/*",
-          type: "features",
-        },
-        { type: "views", pattern: "views/**" },
-      ],
-      "import/resolver": {
-        typescript: {
-          alwaysTryTypes: true,
-          project: "./tsconfig.json",
-        },
-      },
-      "boundaries/ignore": ["**/(router|main).ts", "**/*.config.ts"],
-    },
     rules: {
       ...boundariesStrict.rules,
       "boundaries/element-types": [
@@ -67,21 +48,46 @@ export default defineConfig([
           default: "disallow",
           rules: [
             {
-              from: "views",
               allow: ["features"],
+              from: "views",
             },
             {
-              from: "features",
               allow: [
                 "components",
                 ["features", { featureName: "${from.featureName}" }],
               ],
+              from: "features",
             },
           ],
         },
       ],
     },
+    settings: {
+      ...boundariesStrict.settings,
+      "boundaries/elements": [
+        { pattern: "src/**", type: "app" },
+        { pattern: "components/**", type: "components" },
+        {
+          capture: ["featureName"],
+          pattern: "features/*",
+          type: "features",
+        },
+        { pattern: "views/**", type: "views" },
+      ],
+      "boundaries/ignore": ["**/(router|main).ts", "**/*.config.ts"],
+      "import/resolver": {
+        typescript: {
+          alwaysTryTypes: true,
+          project: "./tsconfig.json",
+        },
+      },
+    },
   },
+  // #endregion
+
+  // #region Perfectionist
+  // @ts-expect-error https://github.com/azat-io/eslint-plugin-perfectionist/issues/655
+  perfectionist.configs["recommended-natural"],
   // #endregion
 
   // #region Disable rules
--- src/router.ts
+++ src/router.ts
@@ -1,7 +1,8 @@
 import { createRouter, createWebHistory } from "vue-router";
+
 import FlowscriptView from "./views/FlowscriptView.vue";
 
-const routes = [{ path: "/", component: FlowscriptView }];
+const routes = [{ component: FlowscriptView, path: "/" }];
 
 const router = createRouter({
   history: createWebHistory(),
