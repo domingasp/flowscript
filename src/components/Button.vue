c9134a3b3694d7549402830ba7a5d4e738ad058e
Domingas Pleckauskas <d.pleck@outlook.com>
2025-12-30 16:38:38 +0000

    Add cva and scss

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,11 +1,43 @@
 <script setup lang="ts">
-import { ButtonHTMLAttributes } from "vue";
+import { cva, VariantProps } from "class-variance-authority";
 
-type Props = /* @vue-ignore */ ButtonHTMLAttributes;
+const button = cva("button", {
+  variants: {
+    variant: {
+      primary: "primary",
+      secondary: "secondary",
+    },
+  },
+});
 
-defineProps<Props>();
+type ButtonProps = VariantProps<typeof button>;
+
+withDefaults(
+  defineProps<{
+    variant?: ButtonProps["variant"];
+  }>(),
+  {
+    variant: "primary",
+  }
+);
 </script>
 
 <template>
-  <button v-bind="$attrs"><slot /></button>
+  <button v-bind="$attrs" :class="button({ variant })">
+    <slot />
+  </button>
 </template>
+
+<style lang="scss" scoped>
+.button {
+  padding: $padding-lg;
+}
+
+.primary {
+  background-color: red;
+}
+
+.secondary {
+  background-color: blue;
+}
+</style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,24 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { fileURLToPath } from "node:url";
 
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vite.dev/config/
 export default defineConfig(async () => ({
   plugins: [vue()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "@/styles/_variables.scss" as *;`,
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
