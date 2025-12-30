366367f66c93162bbcbf5bfbdcbff1414dae9d3d
Domingas Pleckauskas <d.pleck@outlook.com>
2025-12-30 18:40:08 +0000

    Theme support

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,13 @@
+<script setup lang="ts">
+import { usePreferredColorScheme } from "@vueuse/core";
+import { watch } from "vue";
+
+const colorScheme = usePreferredColorScheme();
+watch(colorScheme, (scheme) => {
+  document.documentElement.setAttribute("data-theme", scheme);
+});
+</script>
+
 <template>
   <main class="container">
     <RouterView />
