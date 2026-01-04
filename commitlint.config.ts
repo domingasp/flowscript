e66225d25cffefd1dd5094a02c960bdc600260c9
Domingas Pleckauskas <d.pleck@outlook.com>
2026-01-04 13:01:49 +0000

    chore: update commitlint config and commit-msg command

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -3,7 +3,6 @@ import { UserConfig, RuleConfigSeverity } from "@commitlint/types";
 const configuration: UserConfig = {
   extends: ["@commitlint/config-conventional"],
   rules: {
-    "header-max-length": [RuleConfigSeverity.Error, "always", 50],
     "body-max-line-length": [RuleConfigSeverity.Error, "always", 72],
   },
 };
