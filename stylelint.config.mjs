ceee862b44333a3abdd57642efaace8354f03820
Domingas Pleckauskas <d.pleck@outlook.com>
2026-01-04 15:48:46 +0000

    chore: remove scss and stylelint

--- stylelint.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import("stylelint").Config} */
-export default {
-  extends: [
-    "stylelint-config-recommended-scss",
-    "stylelint-config-recommended-vue/scss",
-    "stylelint-config-recess-order",
-  ],
-};
